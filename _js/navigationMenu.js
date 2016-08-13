8852b0c8ca4be252c326294124dd8f51f375557a
Chris Patuzzo <chris@patuzzo.co.uk>
2016-08-13 12:05:48 +0100

    Simplify navigation menu

--- _js/navigationMenu.js
+++ _js/navigationMenu.js
@@ -5,7 +5,6 @@ SentientWebsite.NavigationMenu = function () {
 
   self.initialize = function () {
     highlightCurrentLink();
-    updateMenu();
     bindHandlers();
 
     $("nav").show();
@@ -16,59 +15,21 @@ SentientWebsite.NavigationMenu = function () {
     $("nav a").each(function (index, anchor) {
       if (anchor.href === window.location.href) {
         $(anchor).addClass("current");
-        openSection(anchor);
+        var list = $(anchor).closest("ul");
+        toggleVisibility(list);
       }
     });
-  };
-
-  var openSection = function (anchor) {
-    var list = $(anchor).closest("ul");
-    var header = list.prev();
-    var index = $("nav h4").index(header);
-
-    var visibility = loadVisibility();
-    visibility[index] = true;
-    saveVisibility(visibility);
   };
 
   var bindHandlers = function () {
     $("nav h4").click(function (event) {
       var header = event.target;
-      var index = $("nav h4").index(header);
-
-      toggleVisibility(index);
+      var list = $(header).next("ul");
+      toggleVisibility(list);
     });
   };
 
-  var toggleVisibility = function (index) {
-    var visibility = loadVisibility();
-    visibility[index] = !visibility[index];
-    saveVisibility(visibility);
-
-    updateMenu();
-  };
-
-  var updateMenu = function () {
-    var visibility = loadVisibility();
-
-    $("nav ul").each(function (index, list) {
-      var items = $(list).children();
-
-      if (visibility[index]) {
-        items.show();
-      } else {
-        items.hide();
-      }
-    });
-  };
-
-  var saveVisibility = function (visibility) {
-    var value = JSON.stringify(visibility);
-    sessionStorage.setItem("NavigationMenu.visibility", value);
-  };
-
-  var loadVisibility = function () {
-    var value = sessionStorage.getItem("NavigationMenu.visibility");
-    return JSON.parse(value || "{}");
+  var toggleVisibility = function (list) {
+    $(list).children().toggle();
   };
 };
